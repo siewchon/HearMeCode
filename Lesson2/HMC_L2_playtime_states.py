ea65e5a0edc41206474e78dc35b2b25df81e4ca2
siewchon <siewchon@users.noreply.github.com>
2015-02-05 14:35:47 -0500

    Update HMC_L2_playtime_states.py
    Goal of this exercise is to output a list of US 50 states in a select drop down list.
    
    The comma delimited list of states in the string are copied from liststates.com to avoid manually typing all 50 states; I then converted the string into a list using string.split(', ') function;
    
    To output the lists of state abbreviations and states name zip() built in function is used.
    
    note: the order of the state abbreviations in the ls_States_Abbreviations list must be inline with the position of states in the ls_States list in order to get the correct abbreviation:state back.

--- Lesson2/HMC_L2_playtime_states.py
+++ Lesson2/HMC_L2_playtime_states.py
@@ -7,7 +7,7 @@
 
 ls_States_Abbreviations = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
 str_States = "Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware, District Of Columbia, Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana, Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia, Wisconsin, Wyoming"
-ls_States = str_States.split(", ")
+ls_States = str_States.split(', ')
 #print "list states are {} ".format(ls_States)
 
 # Step 2: Create your loop
